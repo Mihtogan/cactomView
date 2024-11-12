3f43fc71b70135be99d6745a02b7ba10e8c9280e
Mihtogan <shekochihin.misha@mail.ru>
2024-11-12 20:37:17 +0700

    создал вьюгруп с элементами, первично добавил параметры

--- app/src/main/java/com/example/castomview/statusIndicatorViewGroup/StatusIndicatorViewGroup.kt
+++ app/src/main/java/com/example/castomview/statusIndicatorViewGroup/StatusIndicatorViewGroup.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.withStyledAttributes
-import androidx.core.view.size
 import com.example.castomview.R
 
 class StatusIndicatorViewGroup @JvmOverloads constructor(
